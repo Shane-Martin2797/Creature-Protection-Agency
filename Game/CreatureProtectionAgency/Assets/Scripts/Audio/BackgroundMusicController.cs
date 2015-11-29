df8210e240e243d1ddd28a0dfc97c88958365374
Metalavocado <metalavocado.games@gmail.com>
2015-11-29 15:44:59 +1000

    Added a BackgroundController with some required variances, will update in TDD Yet to convert files to text, will update

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/Audio/BackgroundMusicController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicController : SingletonBehaviour<BackgroundMusicController> 
+{
+    protected BackgroundMusicController() {}
+
+    public bool isRandom;
+    public AudioSource music;
+    public AudioClip initialClip;
+    public AudioClip[] musicArray;
+
+    void Start ()
+    {
+        music.PlayOneShot(initialClip);
+    }
+
+    void Update ()
+    {
+        if (!music.isPlaying)
+        {
+            PlayMusic ();
+        }
+    }
+
+    private void PlayMusic ()
+    {
+        if (isRandom)
+        {
+            music.loop = false;
+            int randomNumber = Random.Range(0, musicArray.Length);
+            music.PlayOneShot(musicArray[randomNumber]);
+        }
+        else
+        {
+            music.PlayOneShot(initialClip);
+        }
+    }
+}

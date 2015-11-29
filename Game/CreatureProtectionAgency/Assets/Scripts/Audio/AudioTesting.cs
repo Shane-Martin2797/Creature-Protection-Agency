7a4bf183dac7cb1e29e3ceafc45d7c9d8e0bdf28
Metalavocado <metalavocado.games@gmail.com>
2015-11-29 15:43:37 +1000

    Created a development scene for myself as per our Policies and Procedures Imported SingletonBehaviour asset from UCL Created a SoundEffectController according to TDD, some required variances (will update in TDD)

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/Audio/AudioTesting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTesting : MonoBehaviour 
+{
+    public AudioClip soundClip;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SoundEffectController.Instance.PlaySoundEffect(soundClip);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SoundEffectController.Instance.PlaySoundEffect(soundClip, this.gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SoundEffectController.Instance.PlaySoundEffect(soundClip, new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), Random.Range(0f, 10f)));
+        }
+	}
+}
---
+++ Game/CreatureProtectionAgency/Assets/Scripts/Audio/SoundEffectController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectController : SingletonBehaviour<SoundEffectController> 
+{
+    protected SoundEffectController () {}
+    
+    /// <summary>
+    /// Plays the specified sound effect (2D sound)
+    /// </summary>
+    /// <param name="audioClip"></param>
+    public void PlaySoundEffect (AudioClip audioClip)
+    {
+        //Creates an AudioSource on this GameObject as 2D sounds don't need position.
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Plays a single shot of the audio clip
+        audioSource.PlayOneShot(audioClip/*, sfxVolume*/);
+
+        //Destroys the AudioSource after playing the sound effect.
+        Destroy(audioSource, audioClip.length);
+    }
+
+    /// <summary>
+    /// Plays the specified sound effect (3D sound)
+    /// </summary>
+    /// <param name="audioClip"></param>
+    /// <param name="audioGameObject"></param>
+    public void PlaySoundEffect(AudioClip audioClip, GameObject audioGameObject)
+    {
+        //Creates an AudioSource on the object that needs to produce the sound.
+        AudioSource audioSource = audioGameObject.AddComponent<AudioSource>();
+
+        //Plays a single shot of the audio clip
+        audioSource.PlayOneShot(audioClip/*, sfxVolume*/);
+
+        //Destroys the AudioSource after playing the sound effect.
+        Destroy(audioSource, audioClip.length);
+    }
+
+    /// <summary>
+    /// Plays the specified sound effect (3D sound)
+    /// </summary>
+    /// <param name="audioClip"></param>
+    /// <param name="locationOfSound"></param>
+    public void PlaySoundEffect(AudioClip audioClip, Vector3 locationOfSound)
+    {
+        //Creates a gameObject for the audioSource to be added to.
+        GameObject audioSource = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        //Sets the position of the GameObject
+        audioSource.transform.position = locationOfSound;
+        
+        //Plays a single shot of the audio clip
+        audioSource.gameObject.GetComponent<Renderer>().enabled = false;
+
+        //Adds an AudioSource and plays one shot of the audio clip.
+        audioSource.AddComponent<AudioSource>();
+        audioSource.GetComponent<AudioSource>().PlayOneShot(audioClip/*, sfxVolume*/);
+
+        //Destroys the AudioSource GameObject after playing the sound effect.
+        Destroy(audioSource, audioClip.length);
+    }
+}

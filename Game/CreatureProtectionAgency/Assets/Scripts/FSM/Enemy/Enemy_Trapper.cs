c7e266093529a34b39baef0dd05bb8aeaaa7b17a
Shane Martin <ghoulminy@live.com>
2015-12-01 21:27:31 +1000

    Put stuff in scripts.
    re-factored code
    
    resolved conflicts

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/FSM/Enemy/Enemy_Trapper.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Trapper : EnemyController
+{
+	public float waypointSoftEdge = 0f;
+	public Vector3 currentWaypoint = 0;
+
+	public float timeIdleAtWaypoint = 0f;
+	public float idleTimeRemaining = 0f;
+	
+	public float minX, maxX;
+	public float minZ, maxZ;
+
+	private bool timerReset = false;
+    
+	public override void Start ()
+	{
+		base.Start ();
+	}
+
+	public override void Attack ()
+	{		
+	}	
+	
+	
+	public override void Movement ()
+	{
+		if (Vector3.Distance (transform.position, currentWaypoint) <= waypointSoftEdge) {
+
+			if ((idleTimeRemaining <= 0) && !timerReset) {
+				idleTimeRemaining = timeIdleAtWaypoint;
+				timerReset = true;
+			} else {
+				idleTimeRemaining -= Time.deltaTime;
+			}
+	
+			if ((idleTimeRemaining <= 0) && timerReset) {
+				currentWaypoint = new Vector3 (Random.Range (minX, maxX), 0, Random.Range (minZ, maxZ));
+				timerReset = false;
+			}
+		} else {
+			navAgent.SetDestination (currentWaypoint);
+		}
+	}
+}

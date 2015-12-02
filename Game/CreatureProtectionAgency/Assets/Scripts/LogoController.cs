be9db977d95833e79115b3bc131af6e6a76b6db2
Joel Mehonoshen <joel.mehonoshen@hotmail.com>
2015-12-02 14:25:19 +1000

    Title Changes.

--- Game/CreatureProtectionAgency/Assets/Scripts/LogoController.cs
+++ Game/CreatureProtectionAgency/Assets/Scripts/LogoController.cs
@@ -8,6 +8,7 @@ public static class Scenes
 	public static string logo = "LogoSplash";
 	public static string Title = "Title";
 	public static string Main = "Main";
+	public static string HUD = "HUD";
 }
 
 public class LogoController : MonoBehaviour 

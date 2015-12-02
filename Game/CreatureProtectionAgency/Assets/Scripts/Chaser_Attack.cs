5bc558a9e07fed074e24ff2684ca68dcb0235ea1
Shane Martin <shanemartin@shanes-mbp.sae.au>
2015-12-02 14:23:22 +1000

    Chaser Complete!

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/Chaser_Attack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser_Attack : MonoBehaviour
+{
+	public float lifetimeOfAttack = .2f;
+	float timer;
+	
+	void OnTriggerEnter (Collider col)
+	{
+		Notify (col.gameObject);
+	}
+	
+	void Notify (GameObject gameObj)
+	{
+		Creature creature = gameObj.GetComponent<Creature> ();
+		if (creature != null) {
+			//Kills the creature
+			Destroy (creature.gameObject);
+		}
+	}
+	
+	void Update ()
+	{
+		if (timer <= 0) {
+			this.gameObject.SetActive (false);
+		} else {
+			timer -= Time.deltaTime;
+		}
+	}
+	
+	void OnEnable ()
+	{
+		timer = lifetimeOfAttack;	
+	}
+}
--- Game/CreatureProtectionAgency/Assets/Scripts/FSM/Enemy/Enemy_Chaser.cs
+++ Game/CreatureProtectionAgency/Assets/Scripts/FSM/Enemy/Enemy_Chaser.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Enemy_Chaser : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Enemy_Chaser : EnemyController
+{
+	public float idleTimeDef = .1f;
 	
+	public override void Awake ()
+	{
+		base.Awake ();
+	}
+	public override void Attack ()
+	{
+		attackObject.SetActive (true);
+		idleTime = idleTimeDef;
+	}
+
+	public override void Movement ()
+	{
+		if (PlayerController.Instance.creatureList.Count > 0) {
+			navAgent.SetDestination (GetClosestCreature (PlayerController.Instance.creatureList).transform.position);
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	Creature GetClosestCreature (List<Creature> creatures)
+	{
+		float closestDist = float.MaxValue;
+		int closestCreatureIndex = 0;
+		for (int i = 0; i < creatures.Count; ++i) {
+			float currDist = Vector3.Distance (transform.position, creatures [i].transform.position);
+			if (currDist < closestDist) {
+				closestDist = currDist;
+				closestCreatureIndex = i;
+			}
+		}
+				
+		//Set our target to the creature we are currently checking
+		return creatures [closestCreatureIndex];
 	}
+
 }

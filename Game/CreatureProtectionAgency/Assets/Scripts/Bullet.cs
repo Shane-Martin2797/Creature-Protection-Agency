93d799dc49389c6fa6d4c452fd9dbd0724fcce19
Metalavocado <metalavocado.games@gmail.com>
2015-12-02 11:49:31 +1000

    Added the bullet script Tests if collided with creature adds stun effect

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour 
+{
+    public float stunTime = 1;
+	
+    void OnCollisionEnter (Collision collision)
+    {
+        Creature creatureHit = collision.gameObject.GetComponent<Creature>();
+
+        if (creatureHit != null)
+        {
+            creatureHit.Stun (stunTime);
+        }
+    }
+}

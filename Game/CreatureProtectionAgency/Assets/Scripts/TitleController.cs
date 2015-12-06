709c35240f93b763d0a937e5445892ff7a36041b
Joel Mehonoshen <joel.mehonoshen@hotmail.com>
2015-12-06 17:22:48 +1000

    Added controls, credits and fixed up the title screen functionality.

--- Game/CreatureProtectionAgency/Assets/Scripts/TitleController.cs
+++ Game/CreatureProtectionAgency/Assets/Scripts/TitleController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class TitleController : MonoBehaviour {
 	public GameObject OptionsScreen;
+	public GameObject CreditsScreen;
+	public GameObject ControlsScreen;
+	public GameObject titleObject;
 	// Use this for initialization
 	void Start () {
 		OptionsScreen.SetActive (false);
@@ -13,9 +16,28 @@ public class TitleController : MonoBehaviour {
 	}
 	public void OnClick_Options(){
 		OptionsScreen.SetActive (true);
+		titleObject.SetActive (false);
+		CreditsScreen.SetActive (false);
+		ControlsScreen.SetActive (false);
+
+	}
+	public void OnClick_Credits(){
+		CreditsScreen.SetActive (true);
+		titleObject.SetActive (false);
+		OptionsScreen.SetActive (false);
+		ControlsScreen.SetActive (false);
+	}
+	public void OnClick_Controls(){
+		ControlsScreen.SetActive (true);
+		titleObject.SetActive (false);
+		OptionsScreen.SetActive (false);
+		CreditsScreen.SetActive (false);
 	}
 	public void OnClick_Back(){
 		OptionsScreen.SetActive (false);
+		CreditsScreen.SetActive (false);
+		ControlsScreen.SetActive (false);
+		titleObject.SetActive (true);
 	}
 	public void OnClick_Quit(){
 		Application.Quit ();

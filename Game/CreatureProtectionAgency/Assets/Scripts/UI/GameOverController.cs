c7427663b3c9b7cd187a7e32203c0faf24f5e4bc
Metalavocado <metalavocado.games@gmail.com>
2015-12-14 00:52:16 +1000

    Added and finished implementing the End Game images

---
+++ Game/CreatureProtectionAgency/Assets/Scripts/UI/GameOverController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverController : MonoBehaviour {
+
+    public Image oneCubsImage;
+    public Image twoCubsImage;
+    public Image oneParentsTwoCubsImage;
+    public Image twoParentsTwoCubsImage;
+    public Image fullWinImage;
+
+	// Use this for initialization
+	void Start () 
+    {
+        switch (PlayerController.Instance.creatureList.Count)
+        {
+            case 0:
+                oneCubsImage.enabled = false;
+                twoCubsImage.enabled = false;
+                twoParentsTwoCubsImage.enabled = true;
+                fullWinImage.enabled = false;
+                break;
+            case 1:
+                oneCubsImage.enabled = false;
+                twoCubsImage.enabled = true;
+                twoParentsTwoCubsImage.enabled = false;
+                fullWinImage.enabled = false;
+                break;
+            case 2:
+                oneCubsImage.enabled = false;
+                twoCubsImage.enabled = true;
+                twoParentsTwoCubsImage.enabled = false;
+                fullWinImage.enabled = false;
+                break;
+            case 3:
+                oneCubsImage.enabled = true;
+                twoCubsImage.enabled = false;
+                twoParentsTwoCubsImage.enabled = false;
+                fullWinImage.enabled = false;
+                break;
+            case 4:
+                oneCubsImage.enabled = false;
+                twoCubsImage.enabled = false;
+                twoParentsTwoCubsImage.enabled = false;
+                fullWinImage.enabled = true;
+                break;
+        }
+	}
+}
